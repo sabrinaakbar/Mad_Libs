a27de38e82d257e3c124459f8bb64e367bddb56a
Sabrina Akbar <sabrinaakbar141@gmail.com>
2019-06-02 17:27:35 -0500

    Initial Commit

---
+++ Mad_Libs/main.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<cmath>
+#include<string>
+
+using namespace std;
+
+
+int main ()
+{
+
+	string color, plural, celebrity;
+	cout << "Enter a color : ";
+	getline(cin, color);
+	cout << "Enter a plural noun : ";
+	getline(cin, plural);
+	cout << "Enter a celebrity name : ";
+	getline(cin, celebrity);
+
+	cout << "Roses are " << color << endl;
+	cout << plural << " are Blue" << endl;
+	cout << "I love " << celebrity;
+
+
+	return 0;
+
+
+
+
+
+}
